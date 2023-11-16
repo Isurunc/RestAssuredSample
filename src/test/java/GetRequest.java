6994e579b14753def1e1c40c649c8dc5251f06cb
isuruPiyarathna <isurupi@wso2.com>
2023-11-16 15:02:43 +0530

    Add more tests

--- src/test/java/GetRequest.java
+++ src/test/java/GetRequest.java
@@ -3,12 +3,14 @@ import io.restassured.http.ContentType;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.hasItem;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 
 public class GetRequest {
 
     @Test
-    public void exampleTest() {
+    public void MainTest() {
         given().
                 log().all().
                 when().
@@ -20,4 +22,52 @@ public class GetRequest {
                 statusCode(200).
                 contentType(ContentType.JSON);
     }
+
+    @Test
+    public void Hamcrest_checkStateNameInResponseBody_equalTo() {
+
+        given().
+                when().
+                get("http://zippopotam.us/us/90210").
+                then().
+                assertThat().
+                body("places[0].state", equalTo("California"));
+    }
+
+    //Negative test
+    @Test
+    public void Hamcrest_checkStateNameInResponseBody_equalToNegative() {
+
+        given().
+                when().
+                get("http://zippopotam.us/US/90210").
+                then().
+                assertThat().
+                body("places[0].state", equalTo("CA"));
+    }
+
+    @Test
+    public void requestUsZipCode90210_hasItem() {
+
+        given().
+                when().
+                get("http://zippopotam.us/us/90210").
+                then().
+                assertThat().
+                body("places.'place name'", hasItem("Beverly Hills"));
+    }
+
+
+    @Test
+    public void requestUsZipCode90210_hasSize() {
+
+        given().
+                when().
+                get("http://zippopotam.us/us/90210").
+                then().
+                assertThat().
+                body("places.'place name'", hasSize(1));
+    }
+
+
 }
