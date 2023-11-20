e50df7066094057b6ada8404fb8c43a817440e2f
isuruPiyarathna <isurupi@wso2.com>
2023-11-20 14:23:13 +0530

    Add paramerized test class

---
+++ src/test/java/configs/constants.java
@@ -0,0 +1,17 @@
+package configs;
+
+public class constants {
+
+    // endpoints
+    public static final String MAIN_ENDPOINT_SUFFIX = "http://zippopotam.us";
+
+
+    public enum countries{
+
+        AU,
+        US,
+        SE,
+        PL
+    }
+
+}
